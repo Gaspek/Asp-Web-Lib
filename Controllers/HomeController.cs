123289f840ad2ff19dd4a519f44f8e90e767fd2c
Gaspek <QuadekLoadek@gmail.com>
2025-01-16 04:21:31 +0100

    Zmodyfikowana strona główna :D

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,15 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Asp_Web_Lib.Models;
+using Asp_Web_Lib.ViewModels;
+
 
 namespace Asp_Web_Lib.Controllers
 {
     [Culture]
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var Books = db.Books.ToList();
+            var model = new HomeViewModel()
+            {
+                Books = Books.Select(b => new BookViewModel
+                {
+                    Title = b.Title,
+                    Description = b.Description,
+                    Authors = string.Join(", ", b.Authors.Select(a => a.FirstName)),
+                    CoverImage = b.CoverImage
+                }).ToList()
+            };
+            return View(model);
         }
 
         public ActionResult About()
@@ -27,6 +42,14 @@ namespace Asp_Web_Lib.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using Asp_Web_Lib.Models;
+using Publisher = System.Security.Policy.Publisher;
+
+namespace Asp_Web_Lib.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<BookViewModel> Books { get; set; }
+    }
+
+    public class BookViewModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Title { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+        public string CoverImage { get; set; }
+        public string Authors { get; set; }
+    }
+}
