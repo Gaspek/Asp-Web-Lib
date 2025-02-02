5c78a0d70241390d001a277d3894d8e09bc9c767
Inga Wojtulewicz <inga_2003@op.pl>
2025-02-02 14:33:59 +0100

    Model limitów

---
+++ Models/Limits.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Helpers;
+
+namespace Asp_Web_Lib.Models
+{
+	public class Limits
+	{
+		//id dla zmian limitów
+		[Key]
+		public int IdLimits { get; set; }
+
+		[Required]
+		//Liczba wypożyczonych książek w jednym momencie
+		public int MaxBorrowedBooks {  get; set; }
+
+		[Required]
+		//Liczba ksiązek w kolejce
+		public int MaxWaitingBooks	{ get; set; }
+
+		[Required]
+		//Liczba możliwych przedłużeń
+		public int MaxExtensionNumber { get; set; }
+
+		[Required]
+		//Liczba dni w przedłużeniu/wypożyczeniu
+		public int ExtensionDays { get; set; }
+
+		[Required]
+		//Kwota za dzień opóźnienia
+		public int LoanAmount { get; set; }
+	}
+}
